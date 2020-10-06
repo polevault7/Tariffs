fc930372f60c5702fb12b854954d722947eb2633
polevault7 <whitevol@mail.ru>
2020-10-06 15:36:11 +0300

    Class is prepared

---
+++ src/Tariffs.java
@@ -0,0 +1,16 @@
+public class Tariffs {
+    String id;
+    String name;
+    int cost;
+    int minutes;
+    int noLimitMinutesRussia;
+    int internetTraffic;
+    boolean noLimitSites;
+    String vk;
+    String facebook;
+    String classMates;
+    String whatsUp;
+    String viber;
+    String someSite;
+    String instagram;
+}
